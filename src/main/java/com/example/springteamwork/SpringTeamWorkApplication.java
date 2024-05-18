3b14e1746206ca2e76dcb267d34a189129128170
raphaelzolotarev <raphaelzolotarev@gmail.com>
2024-05-18 05:44:33 +0200

    start with empty architecture

---
+++ src/main/java/com/example/springteamwork/SpringTeamWorkApplication.java
@@ -0,0 +1,13 @@
+package com.example.springteamwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTeamWorkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringTeamWorkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springteamwork/SpringTeamWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springteamwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTeamWorkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
