5c0ea4521331ef9319071f2621791ab6b518d24c
Janan <fatemeh.javdan@student.intecbrussel.be>
2024-05-22 13:57:18 +0200

    add clas post

---
+++ src/main/java/model/Post.java
@@ -0,0 +1,44 @@
+package model;
+import lombok.*;
+import jakarta.persistence.*;
+import org.apache.catalina.User;
+
+
+import java.util.Date;
+import java.util.List;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "post")
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false, length = 250)
+    private String content;
+
+    @ManyToOne
+    @JoinColumn(name = "author_id", nullable = false)
+    private User author;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "update_date")
+    private Date updateDate;
+
+    @ElementCollection
+    @CollectionTable(name = "post_tags", joinColumns = @JoinColumn(name = "post_id"))
+    @Column(name = "tag")
+    private List<String> tags;
+
+
+
+
+}
