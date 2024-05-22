8ba780b247f75d30d1ec2502d509d5aee2638bc5
9elmaz9 <kakbyelmaz@gmail.com>
2024-05-22 13:59:34 +0200

    Comment class added

--- src/main/java/com/example/springteamwork/model/Comment.java
+++ src/main/java/com/example/springteamwork/model/Comment.java
@@ -1,4 +1,28 @@
 package com.example.springteamwork.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import jakarta.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+@Entity
+@Table(name = "comments")
 public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String name;
+    private String email;
+    private String body;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id", nullable = false)
+    private Post post;
 }
