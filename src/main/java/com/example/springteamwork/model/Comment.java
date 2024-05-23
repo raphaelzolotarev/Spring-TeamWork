471cb439211c0447932af9b8dcb7f4639a10673a
raphaelzolotarev <raphaelzolotarev@gmail.com>
2024-05-23 10:04:40 +0200

    error fixed

--- src/main/java/com/example/springteamwork/model/Comment.java
+++ src/main/java/com/example/springteamwork/model/Comment.java
@@ -1,29 +1,45 @@
 package com.example.springteamwork.model;
-
-import lombok.*;
-
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
-
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-@Getter
-@Setter
-
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "comments")
-public class Comment extends AuditModel{
+
+public class Comment extends AuditModel {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    private Long id;
 
-    private String name;
-    private String email;
-    private String body;
+    @NotNull
+    @Lob
+    @Column(name = "text", columnDefinition = "LONGTEXT")
+    private String text;
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name = "post_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JsonIgnore
     private Post post;
+
+    public Comment(Post post) {
+        this.post = post;
+    }
+
+    public Comment(String text, Post post) {
+        this.text = text;
+        this.post = post;
+    }
+
 }
