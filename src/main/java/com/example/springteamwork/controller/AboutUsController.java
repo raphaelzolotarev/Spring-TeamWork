6ed213444c18e4d36c066da0bb0e5a0f8a62b473
Abdulrazak Alyoussef <Abdulrazak255@users.noreply.github.com>
2024-06-03 15:49:40 +0200

    about us page

---
+++ src/main/java/com/example/springteamwork/controller/AboutUsController.java
@@ -0,0 +1,12 @@
+package com.example.springteamwork.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AboutUsController {
+    @GetMapping("/about_us")
+    public String aboutUs() {
+        return "about_us"; // assuming "about_us" is the name of your Thymeleaf templat
+    }
+}
